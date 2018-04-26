23129000b503d1dd7cd475aece1b25f0b9516d7c
Sai Harshith Reddy Gaddam <shrshthreddy@gmail.com>
2018-04-26 21:19:55 +0530

    if-else

---
+++ Python3/Python if-else.py
@@ -0,0 +1,13 @@
+import sys
+n = int(input().strip())
+if (n % 2 != 0) :
+    print("Weird")
+    
+elif n in range(2 , 6) :
+    print("Not Weird")
+
+elif n in range(6 , 21) :
+    print("Weird")
+
+else :
+    print("Not Weird")
