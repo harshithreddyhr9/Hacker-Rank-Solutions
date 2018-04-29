1373b5b8e27484a5588a9576e905f134558568ec
Sai Harshith Reddy Gaddam <shrshthreddy@gmail.com>
2018-04-30 00:16:30 +0530

    Use of static block

---
+++ Java/Introduction/Static Initialiser block.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class Solution{
+
+static int B=0;
+static int H=0;
+static boolean flag=false;
+static {
+    Scanner sc = new Scanner(System.in);
+    B=sc.nextInt();
+    H=sc.nextInt();
+    
+    if(B<=0 || H<=0)
+        System.out.println("java.lang.Exception: Breadth and height must be positive");
+    else{       
+    flag=true;
+    }
+}
+
+public static void main(String args[])
+{
+    if(flag)
+    {
+    int area=B*H;
+    System.out.println(area);
+    }
+}
+}
