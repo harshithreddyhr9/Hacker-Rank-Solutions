88580cc44fbffa6dd3a4a46babe6cbddb992eaed
Sai Harshith Reddy Gaddam <shrshthreddy@gmail.com>
2018-05-02 15:31:56 +0530

    Scope

---
+++ 30 Days of Code/Scope.java
@@ -0,0 +1,50 @@
+/*
+Complete the Difference class by writing the following:
+
+A class constructor that takes an array of integers as a parameter and saves it to the elements instance variable.
+A computeDifference method that finds the maximum absolute difference between any 2 numbers in N and stores it in the maximumDifference instance variable.
+*/
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+
+class Difference {
+  	private int[] elements;
+  	public int maximumDifference;
+    
+    	
+Difference(int[] a )
+{
+       elements = a; // Storing a in elements to access it in computeDifference inside Difference class.
+
+}
+//============Sort the array and difference of the last and the first no.s of the array will give the max difference
+public void computeDifference(){
+    Arrays.sort(elements);
+    maximumDifference = elements[elements.length-1] - elements[0];
+    
+}
+} // End of Difference class
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = sc.nextInt();
+        }
+        sc.close();
+
+        Difference difference = new Difference(a);
+
+        difference.computeDifference();
+
+        System.out.print(difference.maximumDifference);
+    }
+}
