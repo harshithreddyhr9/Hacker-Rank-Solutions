7e7e5f9b12e52838d4c27dd282758344f5ee515e
harshithredduhr9 <shrshthreddy@gmail.com>
2018-05-02 19:58:51 +0530

    Python

---
+++ Python3/Numpy/Arrays.py
@@ -0,0 +1,13 @@
+import numpy
+
+def arrays(arr):
+    
+    a = numpy.array(arr,float)
+    b = a[::-1]
+    return b
+    
+    
+
+arr = input().strip().split(' ')
+result = arrays(arr)
+print(result)
