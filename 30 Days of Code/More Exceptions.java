ffb665dee49be54da480a18f1c6cccfa933a4548
Sai Harshith Reddy Gaddam <shrshthreddy@gmail.com>
2018-05-20 17:22:35 +0530

    throw

---
+++ 30 Days of Code/More Exceptions.java
@@ -0,0 +1,38 @@
+
+import java.util.*;
+import java.io.*;
+
+class Calculator{
+    
+    public int power(final int n, final int p) {
+    if (n < 0 || p < 0) {
+      throw new IllegalArgumentException("n and p should be non-negative");
+    }
+    
+    
+    return (int)(Math.pow(n,p)); // Math.pow returns a double. So we need to downcast.
+  }
+}
+
+class Solution{
+
+    public static void main(String[] args) {
+    
+        Scanner in = new Scanner(System.in);
+        int t = in.nextInt();
+        while (t-- > 0) {
+        
+            int n = in.nextInt();
+            int p = in.nextInt();
+            Calculator myCalculator = new Calculator();
+            try {
+                int ans = myCalculator.power(n, p);
+                System.out.println(ans);
+            }
+            catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+        }
+        in.close();
+    }
+}
