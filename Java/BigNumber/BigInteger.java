622f189552b2208c389552309bbc237bf85c5d8b
harshithredduhr9 <shrshthreddy@gmail.com>
2018-05-31 22:05:04 +0530

    Big integer

---
+++ Java/BigNumber/BigInteger.java
@@ -0,0 +1,54 @@
+/*
+In this problem, you have to add and multiply huge numbers! These numbers are so big that you can't contain them in any ordinary data types like a long integer.
+
+Use the power of Java's BigInteger class and solve this problem.
+
+Input Format
+
+There will be two lines containing two numbers, a and b.
+
+Constraints
+
+a and b are non-negative integers and can have maximum 200 digits.
+
+Output Format
+
+Output two lines. The first line should contain a+b, and the second line should contain . Don't print any leading zeros.
+
+Sample Input
+
+1234
+20
+Sample Output
+
+1254
+24680
+*/
+
+/*
+BigInteger class is used for mathematical operation which
+involves very big integer calculations that are outside the limit of all available primitive data types.
+There is no theoretical limit on the upper bound of the range
+*/
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+   public static void main(String[] args) {
+
+     Scanner sc = new Scanner(System.in);
+     BigInteger a = new BigInteger(sc.next());
+     BigInteger b = new BigInteger(sc.next());
+
+        BigInteger  sum, prod;
+        sum = a.add(b);
+        prod = a.multiply(b);
+        System.out.println( sum);
+        System.out.println( prod);
+        
+    }
+}
